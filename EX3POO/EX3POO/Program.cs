5cc1d504e5c4a7c4afe8499fb36c8539412b5a93
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-17 19:42:59 -0300

    Add files via upload

---
+++ EX3POO/EX3POO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX3POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da diagonal do quadrado: ");
+            double valorDiagonal = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ EX3POO/EX3POO/Quadrado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX3POO
+{
+    internal class Quadrado
+    {
+        private double diagonal;
+
+        public Quadrado(double diagonal)
+        {
+            this.diagonal = diagonal;
+        }
+
+        public double CalcularArea()
+        {
+            double lado = diagonal / Math.Sqrt(2);
+            double area = lado * lado;
+            return area;
+        }
+
+    }
+}
